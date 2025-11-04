472f86bc42cd15c4ec196bd07c2feca9bdd02244
Matis Bogaert <Matis.Bogaert@iMacD.local>
2025-11-04 09:09:25 +0100

    first commit

---
+++ vivesEvents/vivesEventsApp.swift
@@ -0,0 +1,17 @@
+//
+//  vivesEventsApp.swift
+//  vivesEvents
+//
+//  Created by Matis Bogaert on 04/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct vivesEventsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
